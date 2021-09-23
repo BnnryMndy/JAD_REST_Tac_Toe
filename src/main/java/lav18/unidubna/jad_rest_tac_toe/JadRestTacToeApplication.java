823ccaf08b14e66b562ba95e3032063cd493baa0
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-23 13:33:23 +0300

    Initial commit

---
+++ src/main/java/lav18/unidubna/jad_rest_tac_toe/JadRestTacToeApplication.java
@@ -0,0 +1,13 @@
+package lav18.unidubna.jad_rest_tac_toe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadRestTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadRestTacToeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lav18/unidubna/jad_rest_tac_toe/JadRestTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package lav18.unidubna.jad_rest_tac_toe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadRestTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
